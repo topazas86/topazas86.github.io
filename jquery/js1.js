80d0ea4b1b6c1933e6d3c992e27c636f1f929e4f
topazas86 <67161238+topazas86@users.noreply.github.com>
2020-06-19 19:17:11 +0300

    Add files via upload

---
+++ jquery/js1.js
@@ -0,0 +1,34 @@
+var sprizes=["arkela","coffe","pepsi","7up"];
+function btn1(){
+document.getElementById("sprize").innerHTML=sprizes[2];}
+function btn2(){
+document.getElementById("sprize").innerHTML=sprizes[0];}
+function btn3(){
+document.getElementById("sprize").innerHTML=sprizes[1];}
+
+
+function btn4(){
+  var psg= document.getElementById("first").value;
+  var man=document.getElementById("second").value;
+if (psg==="10",man==="9"){
+document.getElementById("test").innerHTML=sprizes[1];}
+else {
+document.getElementById("test").innerHTML="none";}
+}
+function btnd(){
+  document.getElementById("tarea").style.visibility="visible";
+  document.getElementById("tarea").value="لايوجد";
+}
+function btnpc(){
+  document.getElementById("tarea").style.visibility="visible";
+  document.getElementById("tarea").value="لايوجد";
+}function btnps(){
+  document.getElementById("tarea").style.visibility="visible";
+  document.getElementById("tarea").value="لايوجد";
+}function btnbl(){
+  document.getElementById("tarea").style.visibility="visible";
+  document.getElementById("tarea").value="لايوجد";
+}function btnt(){
+  document.getElementById("tarea").style.visibility="visible";
+  document.getElementById("tarea").value="لايوجد";
+}
